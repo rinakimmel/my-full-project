c4a0b0aec545c70ca2059ea5add0297fe110e8d3
mc0583256271-cmd <mc0583256271@gmail.com>
2026-01-11 19:20:13 +0200

    hi

--- application/src/App.jsx
+++ application/src/App.jsx
@@ -7,7 +7,6 @@ import GetItems from './components/GetItems'
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
 function App() {
 
-
   return (
     <>
       <BrowserRouter>
@@ -15,16 +14,7 @@ function App() {
         <Route path="/" element={<Navigate to="/logIn" replace />} />
         <Route path="/logIn" element={<LogIn />} />
         <Route path="/register" element={<Register />} />
-        {/* <Route path="home/users/:userId" element={<Home />}/> */}
-        {/* <Route path="home" element={<Home />}>
-          {/* <Route path="info" element={<Info />} />
-          <Route path="logout" element={<Logout />} /> */}
-          {/* <Route path="users/:userId/albums" element={<GetItems />} />
-          <Route path="users/:userId/photos" element={<GetItems />} />
-          <Route path="users/:userId/todos" element={<GetItems />} />
-        </Route> */} 
         <Route path="/home/users/:userId" element={<Home />}>
-          {/* <Route path="*" element={<Navigate to="home" replace />} /> */}
           <Route path="albums" element={<GetItems />} />
           <Route path="photos" element={<GetItems />} />
           <Route path="todos" element={<GetItems />} />
